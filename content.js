03470dc2a40591815d609c6734c187b1002e86c8
aminsbd1 <aminsbd1@gmail.com>
2020-02-25 18:15:34 +0700

    commit kedua

--- content.js
+++ content.js
@@ -1,25 +1,25 @@
 function enter() {
     setTimeout(function() {
-        //click button send
+        //fungsi untuk click button send
         document.getElementsByClassName('_3M-N-')[0].click();
     }, 2000);
 }
 
-let array = ['6282244252383', '62895358427436'];
-let text = ['Halo Satu', 'Halo Dua'];
+let phone = ['6282244252383', '62895358427436']; // nomor WA
+let text = "Selamat Siang Kak, Mau Konfirmasi Apa Benar Ini Pemilik Nomor ";
 
 let i = 0,
-    max = 2;
+    max = phone.length;
 
 function f() {
     let data = document.createElement('a');
-    data.href = "https://web.whatsapp.com/send?phone=" + array[i] + "&text=" + text[i] + "&source&data";
+    data.href = "https://web.whatsapp.com/send?phone=" + phone[i] + "&text=" + text + phone[i] + "&source&data";
     data.id = "hp-" + i;
     data.setAttribute('onclick', 'enter()');
-
     document.body.appendChild(data);
-    document.getElementById("hp-" + i).click();
 
+    //eksekusi click to phone number
+    document.getElementById("hp-" + i).click();
     i++;
     if (i < max) {
         setTimeout(f, 4000);
